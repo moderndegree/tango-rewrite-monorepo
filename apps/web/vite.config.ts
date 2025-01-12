6a9d7bccbdbb075409c98fa215b4aa5dddbe9fd4
Brian Lewis <brian@moderndegree.com>
2025-01-11 20:24:12 -0600

    first commit

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,13 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+// vite.config.ts
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite(),
+    viteReact(),
+    // ...,
+  ],
+});
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,22 @@
+import eslintPrettierConfig from "eslint-config-prettier";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "19",
+      },
+    },
+  },
+  eslintPrettierConfig,
+];
